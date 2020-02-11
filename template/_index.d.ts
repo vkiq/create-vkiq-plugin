376058597fd130ef43bcbb7978283347e2c74b0d
Il Harper <afanyiyu@hotmail.com>
2020-02-11 14:46:04 +0800

    feat: 🎸 Definitions Update

--- template/_index.d.ts
+++ template/_index.d.ts
@@ -23,39 +23,39 @@ import { EventEmitter } from 'events';
 import { WriteStream } from 'fs';
 
 declare class PluginLogger {
-    private name;
-    private stream;
-    log(message: string): void;
-    err(message: string): void;
-    ok(message: string): void;
-    warn(message: string): void;
-    i(message: string): void;
-    l(message: string): void;
-    inf(message: string): void;
-    info(message: string): void;
-    m(message: string): void;
-    msg(message: string): void;
-    message(message: string): void;
-    blue(message: string): void;
-    b(message: string): void;
-    error(message: string): void;
-    e(message: string): void;
-    fatal(message: string): void;
-    stop(message: string): void;
-    red(message: string): void;
-    r(message: string): void;
-    o(message: string): void;
-    s(message: string): void;
-    success(message: string): void;
-    ready(message: string): void;
-    green(message: string): void;
-    g(message: string): void;
-    wrn(message: string): void;
-    w(message: string): void;
-    warning(message: string): void;
-    orange(message: string): void;
-    yellow(message: string): void;
-    constructor(name: string, stream: WriteStream);
+  private name;
+  private stream;
+  log(message: string): void;
+  err(message: string): void;
+  ok(message: string): void;
+  warn(message: string): void;
+  i(message: string): void;
+  l(message: string): void;
+  inf(message: string): void;
+  info(message: string): void;
+  m(message: string): void;
+  msg(message: string): void;
+  message(message: string): void;
+  blue(message: string): void;
+  b(message: string): void;
+  error(message: string): void;
+  e(message: string): void;
+  fatal(message: string): void;
+  stop(message: string): void;
+  red(message: string): void;
+  r(message: string): void;
+  o(message: string): void;
+  s(message: string): void;
+  success(message: string): void;
+  ready(message: string): void;
+  green(message: string): void;
+  g(message: string): void;
+  wrn(message: string): void;
+  w(message: string): void;
+  warning(message: string): void;
+  orange(message: string): void;
+  yellow(message: string): void;
+  constructor(name: string, stream: WriteStream);
 }
 
 declare class VkiQPluginOptions {
@@ -63,6 +63,7 @@ declare class VkiQPluginOptions {
   log: PluginLogger;
   debug: boolean;
   dispatch: EventEmitter;
+  address: string;
   [K: string]: any;
 }
 
